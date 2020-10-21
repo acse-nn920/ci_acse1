9c027e28f3d1005b8f6526e45c052b4073768a62
asce-nn920 <nn920@ic.ac.uk>
2020-10-21 20:25:01 +0100

    Add computation of pi.

--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -1,12 +1,13 @@
 import pytest
 
-from simple_function import my_sum
-
 def my_sum(iterable):
     tot = 0
     for i in iterable:
         tot += i
     return tot
+
+def factorial(n):
+    return n * factorial(n-1) if n else 1
 
 class TestSimpleFunctions(object):
     '''Class to test our simple functions are working correctly'''
@@ -19,3 +20,13 @@ class TestSimpleFunctions(object):
         '''Test our add function'''
         isum = my_sum(iterable)
         assert isum == expected
+
+    @pytest.mark.parametrize('number, expected', [
+        (5, 120),
+        (3, 6),
+        (1, 1)
+    ])
+    def test_factorial(self, number, expected):
+        '''Test our factorial function'''
+        answer = factorial(number)
+        assert answer == expected
